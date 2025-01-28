a1aa7dca00522a26a7ae1a6d3ddd40ff91224bdb
Erick <erick.kuwahara79@gmail.com>
2025-01-28 19:50:12 -0300

    feat: pagination endpoint example

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common'
 
+import { PrismaService } from '../database/prisma.service'
+
 import { CreateAccountController } from './controllers/create-account.controller'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
-
-import { PrismaService } from '../database/prisma.service'
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 
 @Module({
   controllers: [
     AuthenticateController,
     CreateAccountController,
     CreateQuestionController,
+    FetchRecentQuestionsController,
   ],
   providers: [PrismaService],
 })
